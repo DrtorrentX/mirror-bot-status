5dad02b239010957cae37bfcb060536e154374d4
DrtorrentX <149814480+DrtorrentX@users.noreply.github.com>
2023-12-28 14:34:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from pytz import timezone, utc
 from requests import get as rget
 from telegram.error import RetryAfter
-from telegram.ext.Updater import Updater as tgUpdater
+from telegram.ext import Updater as tgUpdater
 
 basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', handlers=[StreamHandler()], level=INFO)
 
