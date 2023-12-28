2719cc11f0f2ce96e1a12702b976c2be21bd9e83
DrtorrentX <149814480+DrtorrentX@users.noreply.github.com>
2023-12-28 14:00:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from dotenv import load_dotenv
 from pytz import timezone, utc
 from requests import get as rget
 from telegram.error import RetryAfter
-from telegram import Update
-from telegram.ext import Updater, CallbackContext
+from telegram.ext import Updater as tgUpdater
 
 basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', handlers=[StreamHandler()], level=INFO)
 
@@ -77,7 +76,8 @@ FOOTER_MSG = getConfig('FOOTER_MSG') or "🫂 Join: https://t.me/+3XSC23Veq2s2Mm
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
-updater = Updater(token=BOT_TOKEN)
+
+updater = tgUpdater(token=BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 15})
 
 
 def get_readable_time(seconds: int) -> str:
